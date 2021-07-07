86bd436492a6cba494577c34093fce73a3bc0983
MarkenJaden <jjsch1410@gmail.com>
2021-07-07 23:53:55 +0200

    Added Discord.NET and gitignore file

--- LandOfRails Website/Program.cs
+++ LandOfRails Website/Program.cs
@@ -4,8 +4,16 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using LandOfRails_Website.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace LandOfRails_Website
 {
@@ -14,6 +22,7 @@ namespace LandOfRails_Website
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            new Program().MainAsync().GetAwaiter().GetResult();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +31,39 @@ namespace LandOfRails_Website
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        public async Task MainAsync()
+        {
+            var token = await File.ReadAllLinesAsync("Sensitive-data");
+            await using var services = ConfigureServices();
+            var client = services.GetRequiredService<DiscordSocketClient>();
+
+            client.Log += LogAsync;
+            services.GetRequiredService<CommandService>().Log += LogAsync;
+
+            await client.LoginAsync(TokenType.Bot, token[0]);
+            await client.StartAsync();
+
+            services.GetRequiredService<TeamHandlingService>().Register();
+
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+
+            return Task.CompletedTask;
+        }
+
+        private ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton<TeamHandlingService>()
+                .AddSingleton<HttpClient>()
+                .BuildServiceProvider();
+        }
     }
 }
---
+++ LandOfRails Website/Services/TeamHandlingService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LandOfRails_Website.Services
+{
+    public class TeamHandlingService
+    {
+        private readonly DiscordSocketClient _discord;
+        private readonly IServiceProvider _services;
+
+        public TeamHandlingService(IServiceProvider services)
+        {
+            _discord = services.GetRequiredService<DiscordSocketClient>();
+            _services = services;
+        }
+
+        public void Register()
+        {
+            
+        }
+    }
+}
