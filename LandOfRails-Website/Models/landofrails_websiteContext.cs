1aa5063375d92c978d723e68e14d80710883e3d2
MarkenJaden <jjsch1410@gmail.com>
2025-05-25 18:05:24 +0200

    Verbessere Sicherheit der MySQL-Verbindungszeichenfolge
    In der Methode `OnConfiguring` wurde eine Warnung entfernt, die auf die Notwendigkeit hinwies, sensible Informationen aus dem Quellcode zu entfernen. Die MySQL-Verbindungskonfiguration wurde in einen bedingten Block verschoben, der nur ausgeführt wird, wenn `optionsBuilder` nicht konfiguriert ist. Dies erhöht die Sicherheit, indem die Verbindungszeichenfolge nicht mehr direkt im Code sichtbar ist.

--- LandOfRails-Website/Models/landofrails_websiteContext.cs
+++ LandOfRails-Website/Models/landofrails_websiteContext.cs
@@ -23,8 +23,7 @@ namespace LandOfRails_Website.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql($"server=landofrails.net;uid=landofrails_website;pwd={File.ReadAllLines("Sensitive-data")[1]};database=landofrails_website", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.6.12-mariadb"));
+
             }
         }
 
